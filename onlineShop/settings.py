9a69cdc8a784cdab23f5272b3f7a735650bff774
Utek <yangx@utek.com>
2018-08-02 15:28:13 +0800

    更改session设置

--- onlineShop/settings.py
+++ onlineShop/settings.py
@@ -145,3 +145,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# session 设置
+SESSION_COOKIE_AGE = 60 * 30 # 30分钟
+SESSION_SAVE_EVERY_REQUEST = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True # 浏览器关闭
